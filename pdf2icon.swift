4c29c446b4f085d9ae47575a7750d3d71a28eeed
Andreas Verhoeven <ave@aveapps.com>
2020-06-12 22:32:02 +0200

    Added Extra Icon Sizes

--- pdf2icon.swift
+++ pdf2icon.swift
@@ -7,7 +7,6 @@
 //  Created by Andreas Verhoeven on 27/01/2019.
 //  Copyright © 2019 bikkelbroeders. All rights reserved.
 //
-
 import Foundation
 import AppKit
 
@@ -28,17 +27,48 @@ try? FileManager.default.createDirectory(at: outputDir, withIntermediateDirector
 
 let outputSizes:[(String, CGFloat, [CGFloat])] = [
 	// (name, point size of the icon, [scales])
+
+	// iPhone
 	("iPhone-Notification", 20, [2, 3]),
-	("iPhone-Settings", 29, [2, 3]),
+	("iPhone-Settings", 29, [1, 2, 3]),
 	("iPhone-Spotlight", 40, [2, 3]),
+	("iPhone-App", 57, [2, 3]),
 	("iPhone-App", 60, [2, 3]),
-	("iPhone-App", 60, [2, 3]),
+
+	// iPad
 	("iPad-Notifications", 20, [1, 2]),
 	("iPad-Settings", 29, [1, 2]),
 	("iPad-Spotlight", 40, [1, 2]),
+	("iPad-Spotlight", 50, [1, 2]),
+	("iPad-App", 72, [1, 2]),
 	("iPad-App", 76, [1, 2]),
 	("iPad-Pro(12.9inch)", 83.5, [2]),
+
+	// App Store
 	("App Store", 1024, [1]),
+
+	// watch
+	("AppleWatch-NotificationCenter", 24, [2]),
+	("AppleWatch-NotificationCenter", 27.5, [2]),
+
+	("AppleWatch-CompanionSettings-NotificationCenter", 29, [2, 3]),
+
+	("AppleWatch-HomeScreen-38mm", 40, [2]),
+	("AppleWatch-HomeScreen-40_42mm", 44, [2]),
+	("AppleWatch-HomeScreen-44mm", 50, [2]),
+
+	("AppleWatch-ShortLook-38mm", 86, [2]),
+	("AppleWatch-ShortLook-40_42mm", 98, [2]),
+	("AppleWatch-ShortLook-44mm", 108, [2]),
+
+	("AppleWatch-AppStore", 1024, [1]),
+
+	// Mac
+	("Mac-16pt", 16, [1, 2]),
+	("Mac-32pt", 32, [1, 2]),
+	("Mac-128pt", 128, [1, 2]),
+	("Mac-256pt", 256, [1, 2]),
+	("Mac-512pt", 512, [1, 2]),
 ]
 
 for (name, dimensions, scales) in outputSizes {
